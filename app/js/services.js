0406c1ef197f281f76f619b9978eb32c1c45c5ec
unknown <dpanyushkin@DPANYUSHKIN.es-mdcore.net>
2013-09-04 11:36:13 +0400

    Add storage service

--- app/js/services.js
+++ app/js/services.js
@@ -13,27 +13,46 @@ angular.module('vkNewsBrowser.services', ['vkNewsBrowser.utils'])
 
       VK.then(function(vk) {
         vk.api('friends.get',
-          { fields: 'uid,first_name,last_name,photo'/*, test_mode: 1*/ },
-          function(data) {
+          {fields: 'uid,first_name,last_name,photo', test_mode: 1},
+          function(data) { // TODO: add rejection
             deferred.resolve(data.response);
           });
       });
 
       return deferred.promise;
-    }
+    };
 
     this.getNews = function() {
       var deferred = Q.defer();
 
       VK.then(function(vk) {
         vk.api('newsfeed.get',
-          { count: 60, test_mode: 1},
-          function(data) {
-            deferred.resolve(data);
+          {count: 100, test_mode: 1},
+          function(data) { // TODO: add rejection
+            deferred.resolve(data.response.items);
           });
       });
 
       return deferred.promise;
-    }
+    };
 
+  }])
+  .service('storage',
+  ['API', 'Q', 'lodash', function(API, Q, _) {
+    var promise = Q.all([API.getFriends(), API.getNews()])
+      .then(function(friends, news) {
+         _.groupJoin(friends, news,
+           function(f) { return f.uid; },
+           function(n) { return n.source_id; },
+           function(f, nGr) { f.news = nGr; }
+         );
+         return friends;
+      });
+
+    this.getFriends = function(pageNumber, pageSize) {
+      return promise.then(function(friends) {
+        var start = pageNumber * pageSize, end = start + pageSize;
+         return friends.slice(start, end);
+      });
+    };
   }]);
